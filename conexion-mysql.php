6e4d833340418a04813012509d6889a3e97d6baf
nicor72 <nicor72@gmail.com>
2019-03-03 11:27:02 -0300

    Update conexion-mysql.php

--- conexion-mysql.php
+++ conexion-mysql.php
@@ -6,7 +6,7 @@
 	mysql_select_db("patagonia", $conect);
 	
 	*/
-	$conect=mysql_connect("mysql.hostinger.com.ar", "u124717283_nico", "luchin01");
-	mysql_select_db("u124717283_patag", $conect);
+	$conect=mysql_connect("mysql.hostinger.com.ar", "*******", "*****");
+	mysql_select_db("********", $conect);
 	
   ?>
